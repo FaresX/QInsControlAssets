2ac1fd3b9a646fc4cf9d1d9bf1f69cbf813c4a86
FaresX <fyzxst@sina.com>
2024-07-03 14:08:38 +0800

    fix drivers of MercuryIPS

--- ExtraLoad/MercuryIPS.jl
+++ ExtraLoad/MercuryIPS.jl
@@ -27,36 +27,39 @@ MercuryIPS_sigpfldy_get(instr) = split(query(instr, "READ:DEV:GRPY:PSU:SIG:PFLD"
 MercuryIPS_sigfldy_get(instr) = split(query(instr, "READ:DEV:GRPY:PSU:SIG:FLD"), "FLD:")[end][1:end-1]
 
 ###Z Field###
-MercuryIPS_sigrfstz_set(instr, val) = query(instr, "SET:DEV:GRPZ:PSU:SIG:RFST:$val")
+function MercuryIPS_sigrfstz_set(instr, val)
+    parse(Float64, val) > 0.3 && (@warn "no larger than 0.3 T/min !!!"; return)
+    query(instr, "SET:DEV:GRPZ:PSU:SIG:RFST:$val")
+end
 MercuryIPS_sigrfstz_get(instr) = split(query(instr, "READ:DEV:GRPZ:PSU:SIG:RFST"), "RFST:")[end][1:end-3]
 
 MercuryIPS_sigfsetz_set(instr, val) = query(instr, "SET:DEV:GRPZ:PSU:SIG:FSET:$val")
 MercuryIPS_sigfsetz_get(instr) = split(query(instr, "READ:DEV:GRPZ:PSU:SIG:FSET"), "FSET:")[end][1:end-1]
-
-MercuryIPS_sigactnz_set(instr, val) = query(instr, "SET:DEV:GRPZ:PSU:ACTN:$val")
-MercuryIPS_sigactnz_get(instr) = split(query(instr, "READ:DEV:GRPZ:PSU:ACTN"), "ACTN:")[end]
 
 MercuryIPS_sigpfldz_get(instr) = split(query(instr, "READ:DEV:GRPZ:PSU:SIG:PFLD"), "PFLD:")[end][1:end-1]
 
 MercuryIPS_sigfldz_get(instr) = split(query(instr, "READ:DEV:GRPZ:PSU:SIG:PFLD"), "FLD:")[end][1:end-1]
 
 let
-    stabilizationtime::Float64 = 60
+    stabilizationtime::Float64 = 300
     timespent::Float64 = 0
     global function MercuryIPS_zheater_set(instr, val)
         heaterstate = MercuryIPS_zheater_get(instr)
         if timespent == 0 && heaterstate != val
             query(instr, "SET:DEV:GRPZ:PSU:SIG:SWHT:$val")
-            timespent = 0
+            sleep(2)
             t1 = time()
-            @async begin
-                while timespent < stabilizationtime
-                    sleep(1)
-                    timespent = time() - t1
+            hs = MercuryIPS_zheater_get(instr)
+            timespent = hs == val ? time() - t1 : 0
+            if timespent != 0
+                @async begin
+                    while timespent < stabilizationtime
+                        sleep(1)
+                        timespent = time() - t1
+                    end
+                    timespent = 0
                 end
-                timespent = 0
             end
-            sleep(1)
         end
     end
     global MercuryIPS_zheater_get(instr) = split(query(instr, "READ:DEV:GRPZ:PSU:SIG:SWHT"), "SWHT:")[end]
@@ -65,4 +68,12 @@ let
     global MercuryIPS_zheatertime_get(_) = string(stabilizationtime)
 
     global MercuryIPS_zheatertimespent_get(_) = string(floor(Int, timespent))
+
+    global function MercuryIPS_sigactnz_set(instr, val)
+        val == "RTOS" && timespent != 0 && (@warn "waiting for heater on"; return)
+        query(instr, "SET:DEV:GRPZ:PSU:ACTN:$val")
+    end
+    global MercuryIPS_sigactnz_get(instr) = split(query(instr, "READ:DEV:GRPZ:PSU:ACTN"), "ACTN:")[end]
 end
+
+MercuryIPS_heliumlevel_get(instr) = split(query(instr, "READ:DEV:DB1.L1:LVL:SIG:HEL:LEV"), "LEV:")[end]
