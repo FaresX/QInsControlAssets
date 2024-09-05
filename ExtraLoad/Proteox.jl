82eeae6063edbe669f8e0c10a5386b2a09791cde
FaresX <fyzxst@sina.com>
2024-09-05 21:09:06 +0800

    update drivers

--- ExtraLoad/Proteox.jl
+++ ExtraLoad/Proteox.jl
@@ -1,1 +1,4 @@
 Proteox_drimixs_get(instr) = split(query(instr, "oi.decs.temperature_control.DRI_MIX_CL.DRI_MIX_S.temperature"), ',')[5]
+
+Proteox_drimixclheater_set(instr, val) = query(instr, "oi.decs.temperature_control.DRI_MIX_CL.setpoint $val")
+Proteox_drimixclheater_get(instr) = rstrip(join(split(query(instr, "oi.decs.temperature_control.DRI_MIX_CL.setpoint"), ',')[[5, 7]], ','), ')')
