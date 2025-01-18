c47b4256e5d336bf63a8bcee8b10957e1999ab5c
FaresX <fyzxst@sina.com>
2025-01-18 21:38:57 +0800

    update Triton: drivers and widgets

--- ExtraLoad/Triton.jl
+++ ExtraLoad/Triton.jl
@@ -18,8 +18,12 @@ Triton_temperatureT8_get(instr) = split(query(instr, "READ:DEV:T8:TEMP:SIG:TEMP"
 
 Triton_temperatureT13_get(instr) = split(query(instr, "READ:DEV:T13:TEMP:SIG:TEMP"), "TEMP:")[end][1:end-1]
 
-Triton_loopmode_set(instr, val) = query(instr, "SET:DEV:TA:TEMP:LOOP:MODE:$val"; delay=6)
-Triton_loopmode_get(instr) = split(query(instr, "READ:DEV:TA:TEMP:LOOP:MODE"), "MODE:")[end]
+Triton_h2power_set(instr, val) = query(instr, "SET:DEV:H2:HTR:SIG:POWR:$val")
+Triton_h2power_get(instr) = split(query(instr, "READ:DEV:H2:HTR:SIG:POWR"), "POWR:")[end][1:end-2]
+
+### TA
+Triton_taloopmode_set(instr, val) = query(instr, "SET:DEV:TA:TEMP:LOOP:MODE:$val"; delay=6)
+Triton_taloopmode_get(instr) = split(query(instr, "READ:DEV:TA:TEMP:LOOP:MODE"), "MODE:")[end]
 
 Triton_taloopP_set(instr, val) = query(instr, "SET:DEV:TA:TEMP:LOOP:P:$val")
 Triton_taloopP_get(instr) = split(query(instr, "READ:DEV:TA:TEMP:LOOP:P"), "P:")[end]
@@ -32,7 +36,7 @@ Triton_taloopD_get(instr) = split(query(instr, "READ:DEV:TA:TEMP:LOOP:D"), "D:")
 
 function Triton_taloopPID_set(instr, val)
     spval = lstrip.(rstrip.(split(val, ',')))
-    length(spval) == 3 || (@warn "please input P,I,D"; return)
+    length(spval) == 3 || (@warn "please input P,I,D"; return "none")
     p, i, d = spval
     query(instr, "SET:DEV:TA:TEMP:LOOP:P:$p:I:$i:D:$d")
 end
@@ -43,37 +47,34 @@ function Triton_taloopPID_get(instr)
     return "P:$p I:$i D:$d"
 end
 
-Triton_looptset_set(instr, val) = query(instr, "SET:DEV:TA:TEMP:LOOP:TSET:$val")
-Triton_looptset_get(instr) = split(query(instr, "READ:DEV:TA:TEMP:LOOP:TSET"), "TSET:")[end][1:end-1]
+Triton_talooptset_set(instr, val) = query(instr, "SET:DEV:TA:TEMP:LOOP:TSET:$val")
+Triton_talooptset_get(instr) = split(query(instr, "READ:DEV:TA:TEMP:LOOP:TSET"), "TSET:")[end][1:end-1]
 
-Triton_looprange_set(instr, val) = query(instr, "SET:DEV:TA:TEMP:LOOP:RANGE:$val")
-Triton_looprange_get(instr) = split(query(instr, "READ:DEV:TA:TEMP:LOOP:RANGE"), "RANGE:")[end][1:end-2]
-
-Triton_h2power_set(instr, val) = query(instr, "SET:DEV:H2:HTR:SIG:POWR:$val")
-Triton_h2power_get(instr) = split(query(instr, "READ:DEV:H2:HTR:SIG:POWR"), "POWR:")[end][1:end-2]
+Triton_talooprange_set(instr, val) = query(instr, "SET:DEV:TA:TEMP:LOOP:RANGE:$val")
+Triton_talooprange_get(instr) = split(query(instr, "READ:DEV:TA:TEMP:LOOP:RANGE"), "RANGE:")[end][1:end-2]
 
 Triton_taexcitationtype_set(instr, val) = query(instr, "SET:DEV:TA:TEMP:EXCT:TYPE:$val")
 Triton_taexcitationtype_get(instr) = split(query(instr, "READ:DEV:TA:TEMP:EXCT:TYPE"), "TYPE:")[end]
 
 function Triton_taexcitationcur_set(instr, val)
     type = Triton_taexcitationtype_get(instr)
-    type == "CUR" || (@warn "excitation is type of voltage"; return)
+    type == "CUR" || (@warn "excitation is type of voltage"; return "none")
     query(instr, "SET:DEV:TA:TEMP:EXCT:MAG:$val")
 end
 function Triton_taexcitationcur_get(instr)
     type = Triton_taexcitationtype_get(instr)
-    type == "CUR" || (@warn "excitation is type of voltage"; return)
+    type == "CUR" || (@warn "excitation is type of voltage"; return "none")
     split(query(instr, "READ:DEV:TA:TEMP:EXCT:MAG"), "MAG:")[end][1:end-1]
 end
 
 function Triton_taexcitationvolt_set(instr, val)
     type = Triton_taexcitationtype_get(instr)
-    type == "VOLT" || (@warn "excitation is type of current"; return)
+    type == "VOLT" || (@warn "excitation is type of current"; return "none")
     query(instr, "SET:DEV:TA:TEMP:EXCT:MAG:$val")
 end
 function Triton_taexcitationvolt_get(instr)
     type = Triton_taexcitationtype_get(instr)
-    type == "VOLT" || (@warn "excitation is type of current"; return)
+    type == "VOLT" || (@warn "excitation is type of current"; return "none")
     split(query(instr, "READ:DEV:TA:TEMP:EXCT:MAG"), "MAG:")[end][1:end-1]
 end
 
@@ -165,4 +166,104 @@ let
         end
     end
     global Triton_tlm400probeautotsetsw_get(_) = tlm400probeautotsetsw
+end
+
+### T8
+
+Triton_t8loopmode_set(instr, val) = query(instr, "SET:DEV:T8:TEMP:LOOP:MODE:$val"; delay=6)
+Triton_t8loopmode_get(instr) = split(query(instr, "READ:DEV:T8:TEMP:LOOP:MODE"), "MODE:")[end]
+
+Triton_t8loopP_set(instr, val) = query(instr, "SET:DEV:T8:TEMP:LOOP:P:$val")
+Triton_t8loopP_get(instr) = split(query(instr, "READ:DEV:T8:TEMP:LOOP:P"), "P:")[end]
+
+Triton_t8loopI_set(instr, val) = query(instr, "SET:DEV:T8:TEMP:LOOP:I:$val")
+Triton_t8loopI_get(instr) = split(query(instr, "READ:DEV:T8:TEMP:LOOP:I"), "I:")[end]
+
+Triton_t8loopD_set(instr, val) = query(instr, "SET:DEV:T8:TEMP:LOOP:D:$val")
+Triton_t8loopD_get(instr) = split(query(instr, "READ:DEV:T8:TEMP:LOOP:D"), "D:")[end]
+
+function Triton_t8loopPID_set(instr, val)
+    spval = lstrip.(rstrip.(split(val, ',')))
+    length(spval) == 3 || (@warn "please input P,I,D"; return "none")
+    p, i, d = spval
+    query(instr, "SET:DEV:T8:TEMP:LOOP:P:$p:I:$i:D:$d")
+end
+function Triton_t8loopPID_get(instr)
+    p = Triton_t8loopP_get(instr)
+    i = Triton_t8loopI_get(instr)
+    d = Triton_t8loopD_get(instr)
+    return "P:$p I:$i D:$d"
+end
+
+Triton_t8looptset_set(instr, val) = query(instr, "SET:DEV:T8:TEMP:LOOP:TSET:$val")
+Triton_t8looptset_get(instr) = split(query(instr, "READ:DEV:T8:TEMP:LOOP:TSET"), "TSET:")[end][1:end-1]
+
+Triton_t8looprange_set(instr, val) = query(instr, "SET:DEV:T8:TEMP:LOOP:RANGE:$val")
+Triton_t8looprange_get(instr) = split(query(instr, "READ:DEV:T8:TEMP:LOOP:RANGE"), "RANGE:")[end][1:end-2]
+
+Triton_t8excitationtype_set(instr, val) = query(instr, "SET:DEV:T8:TEMP:EXCT:TYPE:$val")
+Triton_t8excitationtype_get(instr) = split(query(instr, "READ:DEV:T8:TEMP:EXCT:TYPE"), "TYPE:")[end]
+
+function Triton_t8excitationcur_set(instr, val)
+    type = Triton_t8excitationtype_get(instr)
+    type == "CUR" || (@warn "excitation is type of voltage"; return "none")
+    query(instr, "SET:DEV:T8:TEMP:EXCT:MAG:$val")
+end
+function Triton_t8excitationcur_get(instr)
+    type = Triton_t8excitationtype_get(instr)
+    type == "CUR" || (@warn "excitation is type of voltage"; return "none")
+    split(query(instr, "READ:DEV:T8:TEMP:EXCT:MAG"), "MAG:")[end][1:end-1]
+end
+
+function Triton_t8excitationvolt_set(instr, val)
+    type = Triton_t8excitationtype_get(instr)
+    type == "VOLT" || (@warn "excitation is type of current"; return "none")
+    query(instr, "SET:DEV:T8:TEMP:EXCT:MAG:$val")
+end
+function Triton_t8excitationvolt_get(instr)
+    type = Triton_t8excitationtype_get(instr)
+    type == "VOLT" || (@warn "excitation is type of current"; return "none")
+    split(query(instr, "READ:DEV:T8:TEMP:EXCT:MAG"), "MAG:")[end][1:end-1]
+end
+
+let
+    t8autotsetsw::String = "OFF"
+    global function Triton_t8autotset_set(instr, val)
+        if t8autotsetsw == "ON"
+            sv = parse(Float64, val)
+            if sv <= 1.5
+                exctcur, looprange = if sv < 0.1
+                    "1nA", "3.16"
+                elseif 0.07 <= sv < 0.7
+                    "10nA", "10"
+                elseif 0.5 <= sv <= 1.5
+                    "31.6nA", "31.6"
+                end
+                loopmode = Triton_t8loopmode_get(instr)
+                loopmode == "OFF" && Triton_t8loopmode_set(instr, "ON")
+                Triton_t8excitationtype_set(instr, "CUR")
+                Triton_t8excitationcur_set(instr, exctcur)
+                # Triton_t8loopPID_set(instr, pid)
+                Triton_t8looprange_set(instr, looprange)
+                # Triton_h2power_set(instr, h2p)
+                Triton_t8looptset_set(instr, val)
+            end
+        end
+    end
+    global Triton_t8autotset_get(instr) = Triton_temperatureT8_get(instr)
+
+    global function Triton_t8autotsetsw_set(instr, val)
+        if val == "ON"
+            t8autotsetsw = "ON"
+        elseif val == "OFF"
+            t8autotsetsw = "OFF"
+            loopmode = Triton_t8loopmode_get(instr)
+            loopmode == "ON" && Triton_t8loopmode_set(instr, "OFF")
+            Triton_t8excitationtype_set(instr, "CUR")
+            Triton_t8excitationcur_set(instr, "316pA")
+            Triton_t8looprange_set(instr, "0")
+            Triton_h2power_set(instr, "8000")
+        end
+    end
+    global Triton_t8autotsetsw_get(_) = t8autotsetsw
 end
