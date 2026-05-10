fed0802f39d607a488c6f64f9c5fca09abb26597
FaresX <fyzxst@sina.com>
2026-05-10 12:42:36 +0800

    update drivers

--- ExtraLoad/AH2500A.jl
+++ ExtraLoad/AH2500A.jl
@@ -1,8 +1,11 @@
+idn_AH2500A(_) = "AH2500A"
 let
-    r = r"C=([ \-+][\w.0-9]*)[ ]*PF L=([ \-+][\w.0-9]*)[ ]*NS V=([ \-+][\w.0-9]*)"
+    r = r"C=([ \-+][\w.0-9]*)[ ]*PF L=([ \-+][\w.0-9]*)[ ]*[ND]S V=([ \-+][\w.0-9]*)"
     global function AH2500A_querydata_get(instr)
         s = query(instr, "Q")
-        C, PFL, NSV = match(r, s)
+        m = match(r, s)
+        isnothing(m) && @error "m is nothing" s=s
+        C, PFL, NSV = m
         return "$C,$PFL,$NSV"
     end
 end
